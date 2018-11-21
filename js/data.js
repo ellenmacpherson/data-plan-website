6081e89265fb90e0fa124f4993137b4ffbb32196
Ellen Macpherson <macphersonellen@outlook.com>
2018-11-21 14:04:15 +0000

    got lone wolf calculator donegit add .git add .git add .!

--- js/data.js
+++ js/data.js
@@ -1,12 +1,36 @@
+var choices = ["3GB", "6GB", "15GB", "30GB", "Monthly", "Annually"];
 
-var choices = ["3GB", "6GB", "15GB", "30GB"];
+var price = 0.00
 
-var dataChoice = 0;
-var contractChoice;
+/*var dataChoice = 0;
+var dataPrice = 0;
+var contractChoice;*/
 
-/*data-price = parsInt(this.getAtrribute.("data-price"));*/
+var choices = ["3GB", "6GB", "15GB", "30GB", "monthly", "annually"];
 
-$(".gbButtons").click(function(event){
+var price = 0;
+
+// wolf price calculations and price display//
+
+$(".wolf-price").click(function(event) {
+  var wolf_gb = this.getAttribute("data-gb");
+  var wolf_price = this.getAttribute("data-price");
+  price = wolf_price;
+  console.log(wolf_price);
+});
+
+$(".month").click(function(event) {
+  $("#price").text(price);
+});
+
+$(".year").click(function(event) {
+  $("#price").text(price * 12);
+});
+
+
+
+
+/*$(".gbButtons").click(function(event) {
   var dataChoice = this.getAttribute("data-gb");
   $("#gb").text(dataChoice);
   return dataChoice;
@@ -18,13 +42,13 @@ $(".wolf-monthly").click(function(event) {
   return contractChoice;
 });
 
+// Everything down to this point works
+
+
 $(".wolf-annually").click(function(event) {
   var contractChoice = this.getAttribute("value");
   $("#contract").text(contractChoice);
   return contractChoice;
 });
 
-$(".calculate-price").click(function(event) {
-
-
-});
+/*data-price = parsInt(this.getAtrribute.("data-price"));*/
