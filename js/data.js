88a6fe2e968a904313fd1c01aa8155536567b7ab
Ellen Macpherson <macphersonellen@outlook.com>
2018-11-21 10:57:55 +0000

    created gb and contract sections

--- js/data.js
+++ js/data.js
@@ -2,8 +2,24 @@
 var choices = ["3GB", "6GB", "15GB", "30GB"];
 
 var dataChoice = 0;
+var contractChoice;
+
+/*data-price = parsInt(this.getAtrribute.("data-price"));*/
 
 $(".gbButtons").click(function(event){
-  var you_pay = this.getAttribute("data-price");
-  $("#you_pay").text(you_pay);
+  var dataChoice = this.getAttribute("data-gb");
+  $("#gb").text(dataChoice);
+  return dataChoice;
+});
+
+$(".wolf-monthly").click(function(event) {
+  var contractChoice = this.getAttribute("value");
+  $("#contract").text(contractChoice);
+  return contractChoice;
+});
+
+$(".wolf-annually").click(function(event) {
+  var contractChoice = this.getAttribute("value");
+  $("#contract").text(contractChoice);
+  return contractChoice;
 });
