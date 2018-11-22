0621148e9d595c1d427f089aa46d74c830b43746
Ellen Macpherson <macphersonellen@outlook.com>
2018-11-22 10:59:34 +0000

    Part way through the third calculator. Trying to think of a formula for the price changes as a result of increases in sim cards

--- js/data.js
+++ js/data.js
@@ -10,6 +10,10 @@ var chatterbox_price =' '
 var mins_number = ' '
 var mins_price = ' '
 var total_chatterbox_price  =' '
+var family_gb =' '
+var family_price =' '
+var family_sim =' '
+var sim_price =' '
 
 $(".contract-question").hide ();
 $(".pay").hide ();
@@ -61,6 +65,42 @@ $(".chatterbox-mins-price").click(function (event) {
   total_price = mins_price + chatterbox_price;
   console.log(total_price);
 
+});
+
+$(".month").click(function(event) {
+  $(".pay-chatterbox").show();
+  $("#price-chatterbox").text(total_price);
+  $("#gb-selection-chatterbox").text(chatterbox_gb + " /month");
+  $("#contract-selection").text(total_price+" /month");
+});
+
+$(".year").click(function(event) {
+  $(".pay-chatterbox").show();
+  $("#price-chatterbox").text((total_price * 12) + " /year");
+  $("#gb-selection-chatterbox").text(chatterbox_gb + " /month");
+  $("#contract-selection").text((total_price) + " /month");
+});
+
+// Family calculator
+
+/*
+$(".contract-question-family").hide();
+$(".pay-family").hide();
+$(".data-question-family").hide();
+*/
+
+$(".sim-cards").click(function(event) {
+  $(".data-question-family").show();
+  family_sim = parseInt(this.getAttribute("data-sim"));
+  console.log(family_sim);
+});
+
+
+$(".family-gb-price").click(function(event) {
+  $(".contract-question-family").show();
+  family_gb = this.getAttribute("data-gb-family");
+  family_price = parseInt(this.getAttribute("data-gb-family-price"));
+  console.log(family_price);
 });
 
 $(".month").click(function(event) {
